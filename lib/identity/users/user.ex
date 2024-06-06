674d5bd750685300f1cfe189ab3d8acd7337e69e
chirchir <chirchir7370@gmail.com>
2024-06-06 07:56:39 +0300

    user schema

---
+++ lib/identity/users/user.ex
@@ -0,0 +1,46 @@
+defmodule Identity.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @mail_regex ~r/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
+
+
+  schema "users" do
+    field :password_hash, :string
+    field :email, :string
+    field :oid, Ecto.UUID
+    field :firstname, :string
+    field :lastname, :string
+    field :password, :string, virtual: true
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :oid, :firstname, :lastname, :password])
+    |> validate_required([:email, :firstname, :lastname, :password])
+    |> validate_format(:email, @mail_regex, message: "invalid email")
+    |> unique_constraint(:oid)
+    |> unique_constraint(:email)
+    |> put_password_hash()
+    |> put_oid()
+  end
+
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{ password: pass}} = changeset) do
+    changeset |>  put_change(:password_hash, Argon2.hash_pwd_salt(pass))
+  end
+  defp put_password_hash(changeset), do: changeset
+
+  defp put_oid(%Ecto.Changeset{valid?: true} = changeset) do
+    case get_field(changeset, :oid) do
+      nil ->  changeset |> put_change(:oid, Ecto.UUID.generate())
+      _ -> changeset
+
+    end
+
+  end
+  defp put_oid(changeset), do: changeset
+end
